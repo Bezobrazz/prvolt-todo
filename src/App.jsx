edb3eb3e858aa90efbbebaac7f9096be1d7fb70d
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-03-09 10:34:26 +0200

    added revers to list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import List from "./components/List";
 function App() {
   return (
     <>
-      <div className="p-10 flex items-center flex-col">
+      <div className="h-screen p-10 flex items-center flex-col">
         <Input />
         <h2 className="text-center mb-3">This is you ToDo list</h2>
         <List />
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,12 +6,18 @@ const List = () => {
   const todos = useSelector(selectTodos);
 
   return (
-    <ul className="w-full border rounded-md p-2 h-1/2 ">
-      {todos.map((todo) => (
-        <li key={nanoid()} className="border rounded-lg p-2 bg-yellow-100 mb-2">
-          <p>{todo}</p>
-        </li>
-      ))}
+    <ul className="w-full border rounded-md p-2 h-1/2 overflow-y-auto">
+      {todos
+        .slice(0)
+        .reverse()
+        .map((todo) => (
+          <li
+            key={nanoid()}
+            className="border rounded-lg p-2 bg-yellow-100 mb-2"
+          >
+            <p>{todo}</p>
+          </li>
+        ))}
     </ul>
   );
 };
