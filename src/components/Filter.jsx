061a3e33bfcec7418f39223e7c90c7086017625f
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2024-03-11 19:52:53 +0200

    added close dropdown by choose filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,24 +1,27 @@
 import { useDispatch } from "react-redux";
 import { setVisibilityFilter } from "../redux/todoSlice";
+import { useRef } from "react";
 const Filter = () => {
+  const detailsRef = useRef(null);
   const dispatch = useDispatch();
 
+  const handleOptionClick = (filter) => {
+    dispatch(setVisibilityFilter(filter));
+    detailsRef.current.removeAttribute("open");
+  };
+
   return (
-    <details className="dropdown">
+    <details ref={detailsRef} className="dropdown">
       <summary className="m-1 btn">Filter ToDo</summary>
       <ul className="px-2  shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
         <li>
-          <a onClick={() => dispatch(setVisibilityFilter("all"))}>All</a>
+          <a onClick={() => handleOptionClick("all")}>All</a>
         </li>
         <li>
-          <a onClick={() => dispatch(setVisibilityFilter("completed"))}>
-            Completed
-          </a>
+          <a onClick={() => handleOptionClick("completed")}>Completed</a>
         </li>
         <li>
-          <a onClick={() => dispatch(setVisibilityFilter("current"))}>
-            Current
-          </a>
+          <a onClick={() => handleOptionClick("current")}>Current</a>
         </li>
       </ul>
     </details>
